55a0bef3fcbcd6f8eb8f9951adfe31fe5af0bca1
saequus <slava@spetsyian.com>
2020-05-03 19:48:02 +0300

    Reworked algorithm

--- Algorithms/ZAlgorithm.py
+++ Algorithms/ZAlgorithm.py
@@ -1,45 +1,55 @@
-p = 'aacecaaa'
-p1 = 'abcdabcdabi'
+class ZClass:
 
-
-def zfun(s):
-    out = []
-    if not s:
+    @staticmethod
+    def z(s: str) -> list:
+        """
+        Z-функция (англ. Z-function) от строки S и позиции x — это длина максимального префикса подстроки,
+        начинающейся с позиции x в строке S, который одновременно является и префиксом всей строки S.
+        :return:
+        """
+        out = []
+        if not s:
+            return out
+        i, slen = 1, len(s)
+        out.append(slen)
+        while i < slen:
+            left, right = 0, i
+            while right < slen and s[left] == s[right]:
+                left += 1
+                right += 1
+            out.append(left)
+            i += 1
         return out
-    i, slen = 1, len(s)
-    out.append(slen)
-    while i < slen:
-        left, right = 0, i
-        while right < slen and s[left] == s[right]:
-            left += 1
-            right += 1
-        out.append(left)
-        i += 1
-    return out
+
+    @staticmethod
+    def naive_z(s):
+        n = len(s)
+        z = [n]
+
+        for i in range(1, n):
+            left = right = i
+            while right < n and s[right - left] == s[right]:
+                right += 1
+            z.append(right - left)
+        return z
+
+    def example(self):
+        first = 'aacecaaa'
+        print(first)
+        print(self.z(first), '\n')
+
+        second = 'abcdabcdabi'
+        print(second)
+        print(self.z(second), '\n')
+
+        third = 'Все о том же поет луна. Все. Все о том же'
+        print(third)
+        print(self.z(third), '\n')
+
+        forth = 'мне причем мне не очень'
+        print(forth)
+        print(self.z(forth), '\n')
 
 
-print(zfun(p))
-print(zfun(p1))
-
-
-def naive_z(string):
-    n = len(string)
-    z = [n]
-
-    for i in range(1, n):
-        L = R = i
-        while R < n and string[R - L] == string[R]:
-            R += 1
-        z.append(R - L)
-    return z
-
-
-fs = 'Все о том же поет луна. Все о том же'
-fss = 'мне причем мне не очень'
-
-x1 = zfun(fs)
-print(x1)
-x2 = zfun(fss)
-print(x2)
-
-
+z_class = ZClass()
+z_class.example()

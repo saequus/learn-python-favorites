ced353136df042d028029a0926eaa7e24f2c1511
saequus <slava@spetsyian.com>
2020-05-04 18:53:15 +0300

    Reworked jump find

--- Algorithms/JumpAlgorithm.py
+++ Algorithms/JumpAlgorithm.py
@@ -1,23 +1,38 @@
-def jump_find(arr, x):
-    n = len(arr)
-    step = math.sqrt(n)
-    prev = 0
-    
-    while arr[int(min(n, step)-1)] < x:
-        prev = step
-        step += step
-        if step > n:
-            return -1
-    while arr[int(prev)] < x:
-        prev += 1
-        if prev == min(step, n):
-            return -1
-        
-        if arr[int(prev)] == x:
-            return prev
-    return -1 
-        
-arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 ] 
-x = 55
+import math
 
-print(jump_find(arr, x))
+
+class Jump:
+
+    @staticmethod
+    def find(arr, target):
+        n = len(arr)
+        step = int(math.sqrt(n))
+        prev = 0
+
+        while arr[int(min(n, step) - 1)] < target:
+            prev = step
+            step += step
+            if step > n:
+                return -1
+
+            if arr[int(prev)] == target:
+                return prev
+        while arr[int(prev)] < target:
+            prev += 1
+            if prev == min(step, n):
+                return -1
+
+            if arr[int(prev)] == target:
+                return prev
+        return -1
+
+
+test_arr = [1, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
+target_element_value = 55
+
+
+jump = Jump()
+print('\n   Find target element with value %s in array: ' % target_element_value)
+print('  ', ' '.join(str(_) for _ in test_arr))
+print('   using jump find algorithm \n')
+print('   Target element index position: ', jump.find(test_arr, target_element_value), '\n')
